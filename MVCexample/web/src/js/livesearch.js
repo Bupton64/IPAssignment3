8de8d4dd09e66c0901764760f8cdd20e21d7453a
Bupton64 <bupton@hotmail.co.nz>
2018-10-24 15:51:46 +1300

    Replacing lost JS.

---
+++ MVCexample/web/src/js/livesearch.js
@@ -0,0 +1,22 @@
+   function showResult(str) {
+
+        if (str.length==0) {
+            document.getElementById("livesearch").innerHTML="";
+            document.getElementById("livesearch").style.border="0px";
+            return;
+        }
+
+        xmlhttp=new XMLHttpRequest();
+
+
+        xmlhttp.onreadystatechange=function() {
+            if (this.readyState==4 && this.status==200) {
+                document.getElementById("livesearch").innerHTML=this.responseText;
+
+            }
+        }
+
+        xmlhttp.open("GET","livesearch.php?q="+str,true);
+
+        xmlhttp.send();
+    }
